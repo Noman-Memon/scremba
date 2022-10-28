5ee588caa64bcb9e4472d9ca5b9db69029d62a48
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-28 17:18:13 +0500

    Add Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Logo from './image/react-logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <div className="App">
+        <header>
+          <nav>
+            <img src={Logo} alt="logo" width="80px" />
+          </nav>
+        </header>
+        <Navbar />
+        <footer>
+          <h1>Its a Footer</h1>
+        </footer>
+      </div>
+    </>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,26 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <div>
       <nav>
         <h1>Bob's Bistro</h1>
-        {/* <ul> */}
+
         <div>
-          <li>Menu</li>
-          <li>About</li>
-          <li>Contact</li>
+          <ul style={{ display: 'inline-grid' }}>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
         </div>
-        {/* </ul> */}
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol style={{ display: 'inline-grid' }}>
+          <li>It's composable</li>
+          <li>It's declarative</li>
+          <li>It's a hireable skill</li>
+          <li>It's actively maintained by skilled people</li>
+        </ol>
       </nav>
     </div>
   )
