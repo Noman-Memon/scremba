dcc3780b5819828afb04ff0d355baf8e010edc72
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-28 12:29:24 +0500

    start Learning React

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <h1>Bob's Bistro</h1>
+        {/* <ul> */}
+        <div>
+          <li>Menu</li>
+          <li>About</li>
+          <li>Contact</li>
+        </div>
+        {/* </ul> */}
+      </nav>
+    </div>
+  )
+}
